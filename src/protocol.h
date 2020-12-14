74c931637bb5ca2e94a77e5b803931d31229a686
TheWolfInstinct <thunderbear38@gmail.com>
2020-12-14 22:22:59 +0100

    MODIFIED check_http to check for an IP packet and then TCP data

--- src/protocol.h
+++ src/protocol.h
@@ -8,8 +8,6 @@
 
 int match_ip(char ip_from_frame[IP_ADDR_LEN_STR], char ip_from_rule[IP_ADDR_LEN_STR])
 {
-  printf("Source ip from frame: %s\n", ip_from_frame);
-  printf("Source ip from rule: %s\n", ip_from_rule);
   if(ip_from_frame == ip_from_rule || strcmp(ip_from_rule, "any") == 0)
   {
     return 1;
@@ -32,23 +30,29 @@ int match_port(int port_from_frame, char port_from_rule[5])
 
 void check_http(ETHER_Frame *frame, Rule rule)
 {
-  if(strstr(frame->data.data.data, "HTTP") != NULL)
+  if(frame->ethernet_type == IPV4)
   {
-    int field_matches = 0;
-    field_matches = field_matches + match_ip(frame->data.source_ip, rule.source_ad);
-    printf("field_matches: %d\n", field_matches);
-    field_matches = field_matches + match_ip(frame->data.destination_ip, rule.destination_ad);
-    printf("field_matches: %d\n", field_matches);
-    field_matches = field_matches + match_port(frame->data.data.source_port, rule.source_po);
-    printf("field_matches: %d\n", field_matches);
-    field_matches = field_matches + match_port(frame->data.data.destination_port, rule.destination_po);
-    printf("field_matches: %d\n", field_matches);
-    if(field_matches == 4)
+    if(frame->data.transport_type == TCP)
     {
-      printf("%ld", sizeof(rule.options) / sizeof(Rule_option));
-    }
-    else {
-      printf("Packet discarded");
+      if(strstr(frame->data.tcp_data.data, "HTTP") != NULL)
+      {
+        int field_matches = 0;
+        field_matches = field_matches + match_ip(frame->data.source_ip, rule.source_ad);
+        printf("field_matches: %d\n", field_matches);
+        field_matches = field_matches + match_ip(frame->data.destination_ip, rule.destination_ad);
+        printf("field_matches: %d\n", field_matches);
+        field_matches = field_matches + match_port(frame->data.tcp_data.source_port, rule.source_po);
+        printf("field_matches: %d\n", field_matches);
+        field_matches = field_matches + match_port(frame->data.tcp_data.destination_port, rule.destination_po);
+        printf("field_matches: %d\n", field_matches);
+        if(field_matches == 4)
+        {
+          printf("%ld", sizeof(rule.options) / sizeof(Rule_option));
+        }
+        else {
+          printf("Packet discarded");
+        }
+      }
     }
   }
 }
